7065c4545d5998a262e43aa4b7aaed9e47b7b8eb
felipegithub <felipefs92@gmail.com>
2013-08-28 12:06:49 -0300

    commit

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+require "bundler/capistrano"
+require "rvm/capistrano"
+
+server "10.12.0.69", :web, :app, :db, primary: true
+
+set :application, "capistrano" # Nome da app
+set :user, "fabsoft" # usuario do SERVIDOR
+set :deploy_to, "/home/#{user}/apps/#{application}" #Local no servidor onde vai ficar o projeto
+set :use_sudo, false # Não executar os Comandos com 'sudo' no inicio
+
+set :scm, "git" # Tipo: git, bitcucket...
+set :repository, "git@github.com:felipegithub/capistrano.git" #URL do repositorio
+set :branch, "master"
+
+set :rvm_ruby_string, :local              # use the same ruby as used locally for deployment
+set :rvm_autolibs_flag, "read-only"  
+
+#Parametros de conexão via ssh:
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup"
+
+#Definição das 'Tarefas para Deploy:'
+namespace :deploy do
+
+	#Cria-se tarefas, como se fossem metodos que vão ser executados conforme o deploy ocorre
+	task :setup_inicial_nginx, roles: :app do
+		sudo "ln -nfs #{current_path}/config/nginx_site.conf /etc/nginx/sites-enabled/#{application}"
+		sudo "service nginx restart"
+		puts "Criou o Link do arquivo de configuração do ngingx"
+	end
+	
+	after "deploy:setup", "deploy:setup_inicial_nginx"
+	
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capistrano::Application.config.secret_token = '8ed5a139a003342154c9162a692787e7fa7e9e4975b9e9c78c0b42ed8dc23dd9e0ac94c7afdaa1dc366299ef480e65ea930c78e63d9d853c813380b5abfc0a9d'
